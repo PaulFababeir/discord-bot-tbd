9b661dc64766391e8144fdfe3658ac37cb347f19
PaulFababeir <pffababeir@up.edu.ph>
2026-05-30 13:36:01 +0800

    chore: cog integration to main

--- main.py
+++ main.py
@@ -5,12 +5,17 @@ from dotenv import load_dotenv
 load_dotenv() # load all the variables from the env file
 bot = discord.Bot()
 
+# List of cogs to load
+cogs_list = [
+    'cogs.general',
+    'cogs.info'
+]
+
+for cog in cogs_list:
+    bot.load_extension(cog)
+
 @bot.event
 async def on_ready():
     print(f"{bot.user} is ready and online!")
-
-@bot.slash_command(name="hello", description="Say hello to the bot")
-async def hello(ctx: discord.ApplicationContext):
-    await ctx.respond("Hey!")
 
 bot.run(os.getenv('TOKEN')) # run the bot with the token
